2ba253bb740e36d826a9e0aff045e15138e0dc91
xubin <xiaowu2302@163.com>
2020-08-07 11:55:28 +0800

    修改页面位置

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('./raffle/views/index.pug');
+  res.render('./raffle/views/index/index.pug');
 });
 
 module.exports = router;
